0a935cb1a569b6fa7fde4f801d8fd42ee8850bc6
Jurij Master <foto888999@gmail.com>
2025-04-24 15:05:58 +0300

    1

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ Route::get('/', function () {
 
 Route::get("/jobs", function () {
     return "Jobs 1";
-});
+})->name("jobs"); //for accwss
 
 //Route::match(['get','post'], "/submit", function () {
 //    return "Submited 1";
@@ -18,4 +18,27 @@ Route::get("/jobs", function () {
 Route::any("/submit", function () {
     return "Submited 1";
     //
+});
+
+Route::get("/test", function () {
+    $url = route('jobs');
+    return "<a href='{$url}'>Path to {$url}</a>";
+});
+
+Route::get("/api/users", function () {
+
+    return [
+        'n1'=>'Name1',
+        'n2'=>'Name2',
+    ];
+
+});
+
+Route::get("/posts/{id}", function (string $id) {
+   return "Post $id";
+})->where('id', '[0-9]+');
+//'[a-zA-z]+'
+
+Route::get("/posts/{id}/comments/{commentId}", function (string $id, string $commentId) {
+    return "Post $id ".$commentId;
 });
