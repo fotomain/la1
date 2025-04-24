f6d828c3aa4fab9d057cdae2467d35fb93a74cf4
Jurij Master <foto888999@gmail.com>
2025-04-24 18:23:28 +0300

    1

---
+++ resources/views/jobs.php
@@ -0,0 +1,19 @@
+
+<!-- http://la1project.test/jobs -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Jobs List</title>
+</head>
+<body>
+<h1>Available Jobs</h1>
+<ul>
+    <li>Web Developer</li>
+    <li>Software Engineer</li>
+    <li>System Analyst</li>
+    <li>Database Administrator</li>
+</ul>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -8,35 +8,7 @@ Route::get('/', function () {
 });
 
 Route::get("/jobs", function () {
-    return "Jobs 1";
+    return view('jobs');
 })->name("jobs"); //for accwss
 
-Route::get("/test1", function () {
-    return response("Jobs 2",200);
-});
-
-Route::get("/test2", function () {
-    return response("Page not found",404);
-});
-
-Route::get("/test3", function () {
-    return response('<h1>Hello!</h1>',200)->header('Content-Type', 'text/html');
-});
-
-Route::get("/test4", function () {
-    return response()->json(['name' => 'John Doe']);
-});
-
-Route::get("/test5", function () {
-    return response()->download(public_path('favicon.ico'));
-});
-
-Route::get("/test6", function () {
-    return response()->json(['name' => 'John Doe'])->cookie('name', 'John Doe');
-});
-
-Route::get("/test7", function (Request $request) {
-     $val = $request->cookie('name');
-     return response()->json(['$val' => $val]);
-});
 
