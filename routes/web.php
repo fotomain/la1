151d96337d70c42fff42dc18e7fe0656bd3439b4
Jurij Master <foto888999@gmail.com>
2025-04-24 14:41:49 +0300

    1

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get("/jobs", function () {
+    return "Jobs 1";
+});
+
+//Route::match(['get','post'], "/submit", function () {
+//    return "Submited 1";
+//    //
+//});
+
+Route::any("/submit", function () {
+    return "Submited 1";
+    //
+});
